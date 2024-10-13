d6515e877cb1326eb03db2697f9b1da771267a4b
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2024-10-13 23:26:04 +0530

    adding the sql files

--- db_updated_schema.sql
+++ db_updated_schema.sql
@@ -1,12 +1,49 @@
 CREATE DATABASE  IF NOT EXISTS `railway_reservation_management_price`;
 USE `railway_reservation_management_price`;
 
+CREATE TABLE `trains` (
+  `train_id` int NOT NULL AUTO_INCREMENT,
+  `train_no` varchar(10) NOT NULL,
+  `train_name` varchar(255) NOT NULL,
+  `train_type` enum('Passenger','Express','Superfast') NOT NULL,
+  `total_seats` int NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`train_id`)
+);
+
+CREATE TABLE `passengers` (
+  `passenger_id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `gender` enum('Male','Female','Other') NOT NULL,
+  `age` int NOT NULL,
+  `mobile_no` varchar(15) NOT NULL,
+  `aadhar_no` varchar(12) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`passenger_id`),
+  UNIQUE KEY `aadhar_no` (`aadhar_no`)
+) ;
+
+CREATE TABLE `stations` (
+  `station_id` int NOT NULL AUTO_INCREMENT,
+  `station_code` varchar(10) NOT NULL,
+  `station_name` varchar(255) NOT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`station_id`),
+  UNIQUE KEY `station_code` (`station_code`)
+) ;
+
+
+
 CREATE TABLE `classes` (
   `class_id` int NOT NULL AUTO_INCREMENT,
   `class_name` enum('Sleeper','AC','General') NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`class_id`)
-) 
+) ;
 CREATE TABLE `fares` (
   `fare_id` int NOT NULL AUTO_INCREMENT,
   `train_id` int NOT NULL,
@@ -23,23 +60,11 @@ CREATE TABLE `fares` (
   CONSTRAINT `fares_ibfk_1` FOREIGN KEY (`train_id`) REFERENCES `trains` (`train_id`) ON DELETE CASCADE,
   CONSTRAINT `fares_ibfk_2` FOREIGN KEY (`class_id`) REFERENCES `classes` (`class_id`)ON DELETE CASCADE,
   CONSTRAINT `fares_ibfk_3` FOREIGN KEY (`start_station_id`) REFERENCES `stations` (`station_id`) ON DELETE CASCADE,
-  CONSTRAINT `fares_ibfk_4` FOREIGN KEY (`end_station_id`) REFERENCES `stations` (`station_id`)ON DELETE CASCADE
-) 
+  CONSTRAINT `fares_ibfk_4` FOREIGN KEY (`end_station_id`) REFERENCES `stations` (`station_id`) ON DELETE CASCADE
+) ;
 
 
-CREATE TABLE `passengers` (
-  `passenger_id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
-  `gender` enum('Male','Female','Other') NOT NULL,
-  `age` int NOT NULL,
-  `mobile_no` varchar(15) NOT NULL,
-  `aadhar_no` varchar(12) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`passenger_id`),
-  UNIQUE KEY `aadhar_no` (`aadhar_no`)
-) 
+
 
 -- 1403028831
 CREATE TABLE `reservations` (
@@ -68,7 +93,7 @@ CREATE TABLE `reservations` (
   CONSTRAINT `reservations_ibfk_4` FOREIGN KEY (`train_start_station_id`) REFERENCES `stations` (`station_id`)ON DELETE CASCADE,
   CONSTRAINT `reservations_ibfk_5` FOREIGN KEY (`destination_station_id`) REFERENCES `stations` (`station_id`) ON DELETE CASCADE,
   CONSTRAINT `reservations_ibfk_6` FOREIGN KEY (`fare_id`) REFERENCES `fares` (`fare_id`) ON DELETE CASCADE
-) 
+) ;
 
 CREATE TABLE `routes` (
   `route_id` int NOT NULL AUTO_INCREMENT,
@@ -82,7 +107,7 @@ CREATE TABLE `routes` (
   KEY `station_id` (`station_id`),
   CONSTRAINT `routes_ibfk_1` FOREIGN KEY (`train_id`) REFERENCES `trains` (`train_id`) ON DELETE CASCADE,
   CONSTRAINT `routes_ibfk_2` FOREIGN KEY (`station_id`) REFERENCES `stations` (`station_id`) ON DELETE CASCADE
-) 
+) ;
 
 CREATE TABLE `seats` (
   `seat_id` int NOT NULL AUTO_INCREMENT,
@@ -98,23 +123,4 @@ CREATE TABLE `seats` (
   CONSTRAINT `seats_ibfk_1` FOREIGN KEY (`train_id`) REFERENCES `trains` (`train_id`) ON DELETE CASCADE,
   CONSTRAINT `seats_ibfk_2` FOREIGN KEY (`class_id`) REFERENCES `classes` (`class_id`),
   CONSTRAINT `seats_ibfk_3` FOREIGN KEY (`reservation_id`) REFERENCES `reservations` (`reservation_id`) ON DELETE SET NULL
-) 
-CREATE TABLE `stations` (
-  `station_id` int NOT NULL AUTO_INCREMENT,
-  `station_code` varchar(10) NOT NULL,
-  `station_name` varchar(255) NOT NULL,
-  `city` varchar(100) DEFAULT NULL,
-  `state` varchar(100) DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`station_id`),
-  UNIQUE KEY `station_code` (`station_code`)
-) 
-CREATE TABLE `trains` (
-  `train_id` int NOT NULL AUTO_INCREMENT,
-  `train_no` varchar(10) NOT NULL,
-  `train_name` varchar(255) NOT NULL,
-  `train_type` enum('Passenger','Express','Superfast') NOT NULL,
-  `total_seats` int NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`train_id`)
-)
+) ;
